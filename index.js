6cb0f3a2e9bb99cfb36dca65f3e01e9f8dd19cd6
mike <youbob1212@gmail.com>
2021-07-21 01:34:28 -0700

    First bit

---
+++ index.js
@@ -0,0 +1,34 @@
+function getNum(){
+    return Math.floor(Math.random() * 10) + 1; 
+}
+
+
+class NumPicker extends React.Component{
+    render(){
+        const num = getNum()
+        let msg; 
+        if(num ===7) {
+            msg = <div>
+                <h2>CONGRATS YOU WIN! </h2>
+                <img src="https://i.giphy.com/media/nXxOjZrbnbRxS/giphy.webp"/>
+            </div>
+        } else {
+            msg = <div><h2>Sorry you lose</h2></div>
+        }
+
+        return (
+            <div>
+                <h1>Your number is: {num}</h1>
+                <p>{num==7 ? 'Lucky!' : 'Unlucky!'}</p>
+
+                {
+                    msg
+
+                }
+
+            </div>
+        ); 
+    }
+}
+
+ReactDOM.render(<NumPicker/>, document.getElementById('root')); 
